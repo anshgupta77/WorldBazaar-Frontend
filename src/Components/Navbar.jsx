8c23b1a900dd131d418ec46923578d01efc01d4a
Dev-using-Ansh <kuamrguptaansh0@gmail.com>
2025-01-15 13:07:28 +0530

    Added Side bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,39 @@
+import React from "react";
+import { useState } from "react";
+import SideMenu from "./SideMenu";
+import menu from "./../assets/menu-bar.png"
+
 const Navbar = () => {
-    return (
-        <div className="bg-[#131921] text-white px-4 py-2 text-sm flex space-x-4">
-            <span className="cursor-pointer hover:underline">All</span>
-            <span className="cursor-pointer hover:underline">Fresh</span>
-            <span className="cursor-pointer hover:underline">MX Player</span>
-            <span className="cursor-pointer hover:underline">Sell</span>
-            <span className="cursor-pointer hover:underline">Best Sellers</span>
-            <span className="cursor-pointer hover:underline">Today's Deals</span>
-            <span className="cursor-pointer hover:underline">Mobiles</span>
-            <span className="cursor-pointer hover:underline">Prime</span>
-            <span className="cursor-pointer hover:underline">Customer Service</span>
-            <span className="cursor-pointer hover:underline">Electronics</span>
-            <span className="cursor-pointer hover:underline">Home & Kitchen</span>
-            <span className="cursor-pointer hover:underline">Amazon Pay</span>
-        </div>
-    );
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  return (
+    <>
+      <div className="bg-gray-800 text-white px-4 py-2 text-sm flex space-x-6">
+        <span
+          className="material-icons text-white -mr-5 cursor-pointer"
+          onClick={toggleMenu}
+        >
+         <img src={menu} className="w-7 h-5"/>
+        </span>
+        <span className="cursor-pointer hover:underline">All</span>
+        <span className="cursor-pointer hover:underline">Fresh</span>
+        <span className="cursor-pointer hover:underline">MX Player</span>
+        <span className="cursor-pointer hover:underline">Sell</span>
+        <span className="cursor-pointer hover:underline">Best Sellers</span>
+        <span className="cursor-pointer hover:underline">Today's Deals</span>
+        <span className="cursor-pointer hover:underline">Mobiles</span>
+        <span className="cursor-pointer hover:underline">Prime</span>
+        <span className="cursor-pointer hover:underline">Customer Service</span>
+        <span className="cursor-pointer hover:underline">Electronics</span>
+        <span className="cursor-pointer hover:underline">Home & Kitchen</span>
+        <span className="cursor-pointer hover:underline">Amazon Pay</span>
+      </div>
+      <SideMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
+    </>
+  );
 };
 
 export default Navbar;
